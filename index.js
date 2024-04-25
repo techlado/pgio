9b2a30ffba6a1bc8643fb56e5c5d311d205e7c9e
Danielle Gonzalez <15gonzalezdanielle@gmail.com>
2024-04-24 22:11:56 -0700

    First Azure deployment

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const app = express();
 require('dotenv').config();
+const multer = require("multer");
+const upload = multer();
 
 // Add database package and connection string (can remove ssl)
 const { Pool } = require('pg');
@@ -35,6 +37,34 @@ app.get("/", (req, res) => {
     });
   });
 });
+
+// app.get("/input", (req, res) => {
+//   res.render("input");
+// });
+// app.post("/input", upload.single('filename'), (req, res) => {
+//   if (!req.file || Object.keys(req.file).length === 0) {
+//     let message = "Error: Import file not uploaded";
+//     return res.send(message);
+//   };
+//   //Read file line by line, inserting records
+//   const buffer = req.file.buffer;
+//   const lines = buffer.toString().split(/\r?\n/);
+//   lines.forEach(line => {
+//     //console.log(line);
+//     let product = line.split(",");
+//     //console.log(product);
+//     const sql = "INSERT INTO PRODUCT(prod_id, prod_name, prod_desc, prod_price) VALUES($1, $2, $3, $4)";
+//     pool.query(sql, product, (err, result) => {
+//       if (err) {
+//         console.log(`Insert Error. Error message: ${err.message}`);
+//       } else {
+//         console.log(`Inserted successfully`);
+//       }
+//     });
+//   });
+//   let message = `Processing Complete - Processed ${lines.length} records`;
+//   res.send(message);
+// });
 
 // Start listener
 app.listen(process.env.PORT || 3000, () => {
